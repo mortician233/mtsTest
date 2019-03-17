36f56629ec968b4980ccbee31695e22af3b3868b
AntonB <mortician233@gmail.com>
2019-03-17 12:11:32 +0300

    1

--- src/test/java/com/homework/AppTest.java
+++ src/test/java/com/homework/AppTest.java
@@ -1,47 +1,46 @@
 package com.homework;
 
 
-import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
-
-
 public class AppTest
 {
-    WebDriver driver = null;
+    private WebDriver driver = null;
+    private DesiredCapabilities capabilities = null;
     private ChromeOptions options = null;
-    DesiredCapabilities capabilities = new DesiredCapabilities();
-
 
 
     @BeforeClass
     public void initData() {
         System.setProperty("webdriver.chrome.driver",
                 "lib/chromedriver.exe");
-        capabilities.setCapability(CapabilityType.PAGE_LOAD_STRATEGY, "eager");
-        driver = new ChromeDriver();
+        options = new ChromeOptions();
+        options.merge(capabilities);
+        /*
+        Свойство не дожидаться загрузки всей страницы, добавлено из за загрузки динамических элементов, слишком долгое ожидание
+         */
+        options.setPageLoadStrategy(PageLoadStrategy.NONE);
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-
 
     }
 
     @Test()
     public void testCreateForm() {
         driver.navigate().to("https://moskva.mts.ru/personal");
-        driver.findElement(By.xpath("//button[contains(text(),'Да')]")).isDisplayed();
+        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
         driver.findElement(By.xpath("//button[contains(text(),'Да')]")).click();
-        driver.manage().timeouts().implicitlyWait(4,TimeUnit.SECONDS);
         driver.findElement(By.xpath("//input[@name='number']")).sendKeys("9023930349");
         driver.findElement(By.xpath("/html/body/div[3]/div/div[1]/section[1]/div/div[1]/div/div/div/div[2]/div[1]/form/div[2]/div[3]/button")).click();
         WebElement elementNumber
@@ -49,6 +48,7 @@ public class AppTest
         elementNumber.isEnabled();
         WebElement elementSum = driver.findElement(By.xpath("//input[@type='tel' and @name='Sum' and @value='500,00']"));
         elementSum.isEnabled();
+        System.out.println(driver.getCurrentUrl().equals("https://payment.mts.ru/"));
     }
 
     @AfterClass
